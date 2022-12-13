89f3b8b556722717769bb29c16a74ea83847075a
dramtar <dramtar@gmail.com>
2022-12-12 23:12:15 -0500

    - refactoring and optimizations

--- app/src/main/java/com/dramtar/billscollecting/presenter/MainViewModel.kt
+++ app/src/main/java/com/dramtar/billscollecting/presenter/MainViewModel.kt
@@ -196,7 +196,7 @@ class MainViewModel @Inject constructor(
                     billListState = billListState.copy(
                         bills = billsList,
                         totalSum = sum,
-                        formattedTotalSum = sum.getFtdLocalCur()
+                        formattedTotalSum = sum.getFMTLocalCur()
                     )
                     billListState.bills?.let { bills ->
                         billListState = billListState.copy(
